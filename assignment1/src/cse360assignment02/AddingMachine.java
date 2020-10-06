54c411a81d255090427fb6d9f1788d2373079edc
Hannah <Hannah@DESKTOP-71RE9TK>
2020-10-05 22:01:26 -0700

    New iteration of code with complete methods

--- assignment1/src/cse360assignment02/AddingMachine.java
+++ assignment1/src/cse360assignment02/AddingMachine.java
@@ -7,37 +7,48 @@ package cse360assignment02;
 
 public class AddingMachine {
 	 private int total;
+	 private String memory;
 	 /**constructor for AddingMachine class
 	  */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    memory ="0";
 	  }
 	  /**returns the total
 	   * @return an int representing the total added
 	   */
 	  
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  /**adds parameter to total value
 	   * @param value User input int 
 	   */
 	  public void add (int value) {
+		  total=value+total;
+		  memory += " + " + value;
 	  }
+	
 	  /**subtracts parameter from total value
 	   * @param value User input int 
 	   */
+	 
 	  public void subtract (int value) {
+		  total=total-value;
+		  memory += " - " + value;
 	  }
 	  /**prints out all functions done with Adding Machine
 	   * @return a string showing all functions 
 	   */
 	  public String toString () {
-	    return "";
+		  
+	    return "0" + memory;
 	  }
 	  /**clears AddingMachine
 	   */
 	  public void clear() {
+		  memory= "0";
+		  total=0;
 	  }
 
 }
