e52fc01972f7f6d3e4be0551ee19bf9d8cfaf05a
Hannah <Hannah@DESKTOP-71RE9TK>
2020-10-05 19:27:05 -0700

    initial commit of project.

---
+++ assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,43 @@
+package cse360assignment02;
+/**represents an adding machine 
+ * @author Hannah leNoir
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class AddingMachine {
+	 private int total;
+	 /**constructor for AddingMachine class
+	  */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  /**returns the total
+	   * @return an int representing the total added
+	   */
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  /**adds parameter to total value
+	   * @param value User input int 
+	   */
+	  public void add (int value) {
+	  }
+	  /**subtracts parameter from total value
+	   * @param value User input int 
+	   */
+	  public void subtract (int value) {
+	  }
+	  /**prints out all functions done with Adding Machine
+	   * @return a string showing all functions 
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  /**clears AddingMachine
+	   */
+	  public void clear() {
+	  }
+
+}
